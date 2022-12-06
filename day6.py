58b9a72a9431bcc6916aa2fa63881cb5978e6456
Cory Sohrakoff <cory_sohrakoff@hotmail.com>
2022-12-05 21:35:25 -0800

    Day 6

--- day6.py
+++ day6.py
@@ -1,6 +1,13 @@
 with open('input-6.txt', 'r') as f:
     lines = [line.rstrip('\n') for line in f.readlines()]
 
+def getUniqueSequenceCharCount(packet, sequenceLength):
+    for i in range(sequenceLength, len(packet)):
+        if len(set(packet[i-sequenceLength:i])) == sequenceLength:
+            return i
+
 # Part 1
+print(f'Part 1: {getUniqueSequenceCharCount(lines[0], 4)}')
 
 # Part 2
+print(f'Part 2: {getUniqueSequenceCharCount(lines[0], 14)}')
